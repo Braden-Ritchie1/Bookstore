5c667af2a5b67150b89ba842407fce88283d9db6
Braden Ritchie <br333@byu.edu>
2023-02-27 13:50:38 -0700

    added bootsrap styling to the index, _layout, and PaginationtagHelper pages. Still needs some tweaks on the stylings

--- Bookstore/Infastructure/PaginationTagHelper.cs
+++ Bookstore/Infastructure/PaginationTagHelper.cs
@@ -33,17 +33,31 @@ namespace Bookstore.Infastructure
 
         public string PageAction { get; set; }
 
+
+        //classes for bootsrap styling
+        public bool PageClassesEnabled { get; set; } = false;
+        public string PageClass { get; set; }
+        public string PageClassNormal { get; set; }
+        public string PageClassSelected { get; set; }
+
+
         public override void Process (TagHelperContext thc, TagHelperOutput tho)
         {
             IUrlHelper uh = uhf.GetUrlHelper(vc);
 
             TagBuilder final = new TagBuilder("div");
 
-            for (int i = 1; i < PageModel.TotalPages; i++)
+            for (int i = 1; i <= PageModel.TotalPages; i++)
             {
                 TagBuilder tb = new TagBuilder("a");
 
                 tb.Attributes["href"] = uh.Action(PageAction, new { pageNum = i });
+                if (PageClassesEnabled)
+                {
+                    tb.AddCssClass(PageClass);
+                    tb.AddCssClass(i == PageModel.CurrentPage
+                        ? PageClassSelected : PageClassNormal);
+                }
                 tb.InnerHtml.Append(i.ToString());
 
                 final.InnerHtml.AppendHtml(tb);
